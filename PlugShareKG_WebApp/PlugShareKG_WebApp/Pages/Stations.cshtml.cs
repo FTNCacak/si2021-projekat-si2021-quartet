dcfe728d94d0aa63ba4cf45c89ad6d79a62f5ff6
Marko Tomic <masheritto@gmail.com>
2022-01-12 07:18:49 +0100

    Initial commit

---
+++ PlugShareKG_WebApp/PlugShareKG_WebApp/Pages/Stations.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PlugShareKG_WebApp.Pages
+{
+    public class StationsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
