6512dc2d98e52b504875e8a31a090afb7078197a
DESKTOP-CIB2LPP\Nemanja Riznić <114-2019@ftn.edu.rs>
2022-01-14 00:19:08 +0100

    Obrisan ".vs" folder iz projektnog dela

---
+++ Business/BazneStaniceBusiness.cs
@@ -0,0 +1,36 @@
+﻿using ePunjačiData;
+using Shered.BiznisInterfejsi;
+using Shered.Modeli;
+using System;
+using System.Collections.Generic;
+
+namespace Business
+{
+    public class BazneStaniceBusiness : IBazneStaniceBusiness
+    {
+        private readonly BazneStaniceRepository bazneStaniceRepository;
+        public BazneStaniceBusiness()
+            {
+                bazneStaniceRepository = new BazneStaniceRepository(); 
+            } 
+        public bool DodajBaznaStanica(BaznaStanica bs)
+        {
+            return bazneStaniceRepository.DodajBaznaStanica(bs);
+        }
+
+        public bool IzmeniBaznaStanica(BaznaStanica bs)
+        {
+            return bazneStaniceRepository.IzmeniBaznaStanica(bs);
+        }
+
+        public bool ObrišiBaznaStanica(string id_stanice)
+        {
+            return bazneStaniceRepository.ObrišiBaznaStanica(id_stanice);
+        }
+
+        public List<BaznaStanica> SveBazneStanice()
+
+            => bazneStaniceRepository.SveBazneStanice();
+         
+    }
+}
